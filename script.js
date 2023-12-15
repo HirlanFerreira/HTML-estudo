e3040b39c1d8accc5cd115cb1f034e5385561bc7
HirlanFerreira <hirlanreis17@gmail.com>
2023-12-15 15:19:57 -0300

    mensagem

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+ const html = document.documentElement
+ html.classList.toggle("light")
+
+ // pegar a tag imag
+
+ const img = document.querySelector("#profile img")
+ 
+ // substituir a imagem
+
+ if(html.classList.contains("light")) {
+
+   // se tiver light mode, adicionar a imagem light
+
+  imag.setAttribute('src','./assets/avatar-light')
+ } else {
+
+  //  se tiver sem light mode, manter a imagem normal
+
+  imag.setAttribute('src','./assets/avatar')
+ }
+
+
+
+}
